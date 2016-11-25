ecb482d4c751521ecec070f26a0e42dc8e582502
Young Ju Park <parkyngj@gmail.com>
2016-11-24 23:01:25 -0800

    Add linked list node class

---
+++ linked_lists/delete_node.rb
@@ -0,0 +1,3 @@
+require_relative 'linked_list_node'
+
+# Delete a node from a singly-linked list, given only a variable pointing to that node.
---
+++ linked_lists/linked_list_node.rb
@@ -0,0 +1,9 @@
+class LinkedListNode
+
+  attr_accessor :value, :next
+
+  def initialize(value)
+    @value = value
+    @next = nil
+  end
+end
