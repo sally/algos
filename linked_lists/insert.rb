7b8e62309aacc1ef6e2dcc084d3b425c1eb744c3
Young Ju Park <parkyngj@gmail.com>
2016-11-26 13:04:37 -0800

    Add insert linked list node problem statement and solution

--- linked_lists/insert.rb
+++ linked_lists/insert.rb
@@ -7,14 +7,35 @@ require_relative 'linked_list_node'
 #########################################
 
 ## PSEUDOCODE:
+  # Traverse linked list with couner until position input is reached, then
+  # Change pointer of node to insert to reference the node at that position index
+  # Change pointer of previous node to reference the node to insert
+    # This means we must also store data of previous node to change its pointer
+
+  # Edge cases to consider:
+    # Suppose position exceeds the length of the linked list
+      # Just put the node at the end
+    # Suppose the linked list is empty
+      # Raise an error in the case that the linked list is empty
 
 def insert(head, position, value)
+  raise "The linked list provided was empty" if head.nil?
+
   node = LinkedListNode.new(value)
   counter = 0
+  prev_node = nil
   current_node = head
+
   until counter = position
     counter += 1
-    if current_node.next.nil?
-    current_node = current_node.next
+    if current_node.nil?
+      prev_node.next = node
+    else
+      prev_node = current_node
+      current_node = current_node.next
+    end
   end
 end
+
+# Time complexity: O(n), where n is the length of the linked list.
+# Space complexity: O(1), we have no structures that scale based on the size of the data structure.
