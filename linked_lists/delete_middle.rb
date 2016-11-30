3072c890f6ca21816ce34d28439527db02122bf4
Young Ju Park <parkyngj@gmail.com>
2016-11-30 09:48:28 -0800

    Add problem statement, pseudocode, and big O analysis for delete middle

--- linked_lists/delete_middle.rb
+++ linked_lists/delete_middle.rb
@@ -4,4 +4,9 @@
 
 ###########################################
 
-# 
+# Iterate through the linked list until you find the appropriate node
+# Keeping track of the previous node, set its pointer to the current node's next element.
+
+# Time and space complexity:
+  # Time: Linear time O(n), at worst we will have to go to the second-to-last-element, so it costs n-1 time.
+  # Space: Constant, since we only have to keep track of previous and current nodes.
