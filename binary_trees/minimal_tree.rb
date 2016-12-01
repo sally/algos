fa499bfecb92e4be4dbdb31e189ed663eb3be7db
Young Ju Park <parkyngj@gmail.com>
2016-11-30 20:39:05 -0800

    Add problem statement and some pseudocode to minimal tree

---
+++ binary_trees/minimal_tree.rb
@@ -0,0 +1,12 @@
+# CTCI: Minimal tree
+
+# Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height.
+
+####################################
+
+# Toy example: [1,3,5,8,9]
+
+# Need 1 and 3 to go next to each other, and 5 to be its parent -
+
+# Want to start at midpoint, have that be the head
+  # Look at the two remaining arrays,
