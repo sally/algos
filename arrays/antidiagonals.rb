6f3126fb2c2315b5f7c77e7f54ed997d6e4da5f8
Young Ju Park <parkyngj@gmail.com>
2016-12-02 12:11:10 -0800

    Add antidiagonal solution

--- arrays/antidiagonals.rb
+++ arrays/antidiagonals.rb
@@ -14,61 +14,45 @@
 # 6 2
 # 8
 
-
 matrix = [[9, 3, 2],
           [8, 6, 1],
           [5, 5, 6],
           [1, 2, 8]]
 
-print_diagonal = lambda do |current_column, current_row|
-  if !current_column.zero? || current_row == (num_rows - 1)
-      until current_column.zero? || current_row == (num_rows - 1)
-          print matrix[current_row + 1][current_column - 1]
-          current_column -= 1
-          current_row += 1
-      end
-  end
-end
-
 def print_diagonals(matrix)
-
+   diags = ""
    num_rows = matrix.length
    num_columns = matrix[0].length
 
    first_row = matrix[0]
-   last_column = matrix.map do |row|
-     row[-1]
+   last_column = matrix.map {|row| row[-1]}
+
+   print_diagonal = lambda do |current_column, current_row|
+     if !current_column.zero? || current_row == (num_rows - 1)
+       until current_column.zero? || current_row == (num_rows - 1)
+           diags << matrix[current_row + 1][current_column - 1].to_s + " "
+           current_column -= 1
+           current_row += 1
+       end
+     end
    end
 
   first_row.each_with_index do |element, current_column|
         current_row = 0
-        print element
-
-        if !current_column.zero? || current_row == (num_rows - 1)
-            until current_column.zero? || current_row == (num_rows - 1)
-                print matrix[current_row + 1][current_column - 1]
-                current_column -= 1
-                current_row += 1
-            end
-        end
+        diags << element.to_s + " "
+        print_diagonal.call(current_column, current_row)
+        diags << "\n"
     end
 
     last_column.each_with_index do |element, current_row|
         next if current_row.zero?
-
         current_column = num_columns - 1
-        print element
-
-        if !current_column.zero? || current_row == (num_rows - 1)
-            until current_column.zero? || current_row == (num_rows - 1)
-                print matrix[current_row + 1][current_column - 1]
-                current_column -= 1
-                current_row += 1
-            end
-        end
+        diags << element.to_s + " "
+        print_diagonal.call(current_column, current_row)
+        diags << "\n"
     end
+
+    puts diags
 end
-
-
 
 p print_diagonals(matrix)
