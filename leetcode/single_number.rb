f820db8d741ff229be92db35bec8deef467cbca1
Young Ju Park <parkyngj@gmail.com>
2016-12-14 10:42:46 -0800

    add solution for single number

--- leetcode/single_number.rb
+++ leetcode/single_number.rb
@@ -4,3 +4,17 @@
 
 # Note:
 # Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+def single_number(nums)
+  histogram = Hash.new(0)
+
+  nums.each do |num|
+    if histogram[num].zero?
+      histogram[num] += 1
+    else histogram[num] == 1
+      histogram.delete(num)
+    end
+  end
+
+  histogram.keys.first
+end
