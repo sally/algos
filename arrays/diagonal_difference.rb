10512aa5358a31a305cce532a2459096d015b20b
Young Ju Park <parkyngj@gmail.com>
2016-12-16 09:26:12 -0800

    removes silly p statements and submit correct solution

--- arrays/diagonal_difference.rb
+++ arrays/diagonal_difference.rb
@@ -32,8 +32,10 @@ def diagonal_difference(n, matrix)
   end
 
   n.times do |index|
-    anti_diagonal_sum += matrix[n - index][index]
+    anti_diagonal_sum += matrix[n - index - 1][index]
   end
 
   return [main_diagonal_sum - anti_diagonal_sum, anti_diagonal_sum - main_diagonal_sum].max
 end
+
+p diagonal_difference(3, [[11, 2, 4], [4, 5, 6], [10, 8, -12]])
