f29088bf85cf20457fcfbb323177acda613bd4d2
Young Ju Park <parkyngj@gmail.com>
2017-01-10 13:30:23 -0800

    adds problem statement of get random node

---
+++ binary_trees/random_element.rb
@@ -0,0 +1,3 @@
+# Source: CTCI (6th edition, 4.11)
+
+# Random Node: You are implementing a binary tree class from scratch which, in addition to insert, find, and delete, has a method getRandomNode() which returns a random node from te tree. All nodes should be equally likely to be chosen. Design and implement an algorithm for getRandomNode, and explain how you would implement the rest of the method.
