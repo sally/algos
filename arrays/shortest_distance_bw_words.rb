3499a5d6c1dc96aef0622b7dd36797cccf6909c0
Young Ju Park <parkyngj@gmail.com>
2017-01-09 21:11:53 -0800

    adds pseudocode idea for shortest distance bw words

--- arrays/shortest_distance_bw_words.rb
+++ arrays/shortest_distance_bw_words.rb
@@ -7,3 +7,7 @@ require_relative 'minimal_differences_bw_sorted'
 # For example, Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
 #
 # Given word1 = "coding", word2 = "practice", return 3. Given word1 = "makes", word2 = "coding", return 1.
+
+# idea: create indices hash, keys are words and values are arrays of indices
+# find shorest distance between 2 numbers in 2 sorted arrays
+# if two words are equal, then problem becomes finding the shortest distance between two distinct numbers in array
