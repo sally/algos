f3b19c80870ecba7d83274ef4de6cf99df7addfc
Young Ju Park <parkyngj@gmail.com>
2017-01-09 21:11:38 -0800

    adds shortest distance bw words problem

---
+++ arrays/shortest_distance_bw_words.rb
@@ -0,0 +1,9 @@
+require_relative 'minimal_differences_bw_sorted'
+
+# Source: [Leetcode](https://tonycao.gitbooks.io/leetcode-locked/content/LeetCode%20Locked/c1.4.html)
+#
+# Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+#
+# For example, Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+#
+# Given word1 = "coding", word2 = "practice", return 3. Given word1 = "makes", word2 = "coding", return 1.
