b35fcc84553c7b72fb7523e36137931b33561ae2
Young Ju Park <parkyngj@gmail.com>
2017-01-09 16:09:24 -0800

    adds problem statement for transform word

---
+++ graphs/transform_word.rb
@@ -0,0 +1,11 @@
+# Source: LiveRamp Interview
+#
+# Given a source word, target word and an English dictionary, transform the source word to target by changing/adding/removing 1 character at a time, while all intermediate words being valid English words. Return the transformation chain which has the smallest number of intermediate words.
+#
+# e.g. If dictionary = ['cat', 'bat', 'bet', 'bed', 'at', 'ad, 'ed']
+#
+# Then transform_word('cat', 'bed') returns ['cat', 'bat', 'bet', 'bed'].
+#
+# If dictionary = ['cat, 'bat', 'bed', 'at', 'ad', 'ed']
+#
+# Then transform_word('cat', 'bed') returns ['cat', 'at', 'ad', 'ed', 'bed'].
