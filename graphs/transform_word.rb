afb74d25409e721ef5a317a59bd16f78eb29d0ac
Young Ju Park <parkyngj@gmail.com>
2017-01-09 16:09:41 -0800

    adds some pseudocode for transform word

--- graphs/transform_word.rb
+++ graphs/transform_word.rb
@@ -9,3 +9,10 @@
 # If dictionary = ['cat, 'bat', 'bed', 'at', 'ad', 'ed']
 #
 # Then transform_word('cat', 'bed') returns ['cat', 'at', 'ad', 'ed', 'bed'].
+
+# make sure we can assume that both of the words given are actually in the list of words given
+# is this case sensitive?
+# can we assume that a shortest path actually exists?
+
+# build a graph where the values of the nodes contain the words, and there exists an edge between two nodes if and only if their "edit distance" is 1
+# do a breadth first search in the graph starting at word1, look for shortest path to word2
