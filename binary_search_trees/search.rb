867eeffa3a5fe3c6939e613fbf191c26ca530338
Young Ju Park <parkyngj@gmail.com>
2017-01-18 09:56:01 -0800

    add alternate code for search

--- binary_search_trees/search.rb
+++ binary_search_trees/search.rb
@@ -2,17 +2,34 @@ require_relative 'binary_tree_node'
 
 # To search if a given key is in a binary search tree, we first compare it with root. If the key is present at root, we return root. If key is greater than root's key, we recur for right subtree of root node. Otherwise, we recur of left subtree.
 
-def search(root, value)
-  return false if root.nil?
+# This initial version works as well, but doesn't look as clean as the one below
+# def search(root, value)
+#   return false if root.nil?
+#
+#   current_node = root
+#
+#   if current_node.value == value
+#     return true
+#   elsif current_node.value < value
+#     return search(current_node.right, value)
+#   else
+#     return search(current_node.left, value)
+#   end
+# end
 
-  current_node = root
-
-  if current_node.value == value
-    return true
-  elsif current_node.value < value
-    return search(current_node.right, value)
+def search(root, key)
+  if root.nil?
+    return false
   else
-    return search(current_node.left, value)
+    if root.value == key
+      return true
+    else
+      if root.value < key
+        search(root.right, key)
+      else
+        search(root.left, key)
+      end
+    end
   end
 end
 
@@ -40,16 +57,32 @@ sixty_six     = seventy.insert_left(66)
 ninety        = seventy.insert_right(90)
 
 p search(root1,25)
+# => true
 p search(root1,15)
+# => true
 p search(root1,50)
+# => true
 p search(root1,10)
+# => true
 p search(root1,22)
+# => true
 p search(root1,35)
+# => true
 p search(root1,70)
+# => true
 p search(root1,4)
+# => true
 p search(root1,12)
+# => true
 p search(root1,31)
+# => true
 p search(root1,44)
+# => true
 p search(root1,66)
+# => true
 p search(root1,90)
+# => true
 p search(root1,1)
+# => false
+p search(root1,100)
+# => false
