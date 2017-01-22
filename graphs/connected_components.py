6faee4c3af979468dd912d57e10bf49ad7fb5967
Young Ju Park <parkyngj@gmail.com>
2017-01-22 14:00:51 -0800

    adds problem statement for connected component

---
+++ graphs/connected_components.py
@@ -0,0 +1,3 @@
+# In graph theory, a connected component (or just component) of an undirected graph is a subgraph in which any two verties are connected to each other by paths, and which is connected to no additional vertices in the supergraph. - Wikipedia
+
+# Given an undirected graph, we print all connected components line by line. For example, consider graph3.gif in this directory. 
