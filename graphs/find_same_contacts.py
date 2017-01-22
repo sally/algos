666458e82406b3d84cb76267fb3ba40c78258100
Young Ju Park <parkyngj@gmail.com>
2017-01-22 09:53:48 -0800

    add problem idea/pseudocode to same contacts

--- graphs/find_same_contacts.py
+++ graphs/find_same_contacts.py
@@ -17,10 +17,22 @@ contacts = [
 
 # Output:
 
-0 2 3
+"0 2 3"
 
 # Explanation:
 
 # contacts[2] is the same as contact[3] because they both have the same phone number.
 # contact[0] is the same as contact[3] because they both have same email address.
 # therefore, contact 0 and 2 are also the same.
+
+# Create a graph from the contacts list, represented by a dict.
+    # Keys are indices of the contact
+    # Values are contact indices that the key index has "an edge" to
+
+    # Two contact indices have an edge to each other if and only if they have one of the same: phone #, email, or username
+
+# Problem boils down to finding number of connected components in a graph
+    # Iterate through the keys of the graph, doing a DFS for items that are in the same "component"
+        # we keep track of nodes that are already visited
+        # once DFS is finished, we add 1 to number of components and print the components
+    # @ the end, return the number of components
