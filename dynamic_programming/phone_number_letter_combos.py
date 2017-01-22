6e14ef1514968437b48ceebb87411c60f76df0b1
Young Ju Park <parkyngj@gmail.com>
2017-01-22 00:31:52 -0800

    adds problem statement of phone number letter combos

---
+++ dynamic_programming/phone_number_letter_combos.py
@@ -0,0 +1,12 @@
+# Write a program that, given a 7 digit telephone number, print all possible combinations of letters that each number could represent.
+
+digit_map = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+}
