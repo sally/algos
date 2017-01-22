b2430c805f032c91232a4396bb4329ffb4512cde
Young Ju Park <parkyngj@gmail.com>
2017-01-22 00:38:28 -0800

    adds solution for phone number letter combos

--- dynamic_programming/phone_number_letter_combos.py
+++ dynamic_programming/phone_number_letter_combos.py
@@ -10,3 +10,15 @@ digit_map = {
     '8': 'tuv',
     '9': 'wxyz',
 }
+
+def letter_combos(phone_num):
+    combos = ['']
+
+    for k in phone_num:
+        letters = digit_map.get(k, None)
+        if letters:
+            combos = [combo + letter for combo in combos for letter in letters]
+    return combos
+
+print(letter_combos('7033005426'))
+print(letter_combos('5102299071'))
