df5770bcd606ae0a0061ec764b102fb760a0c4fb
Young Ju Park <parkyngj@gmail.com>
2017-01-21 23:26:30 -0800

    adds detect cycle solution

--- graphs/detect_cycle.py
+++ graphs/detect_cycle.py
@@ -13,8 +13,8 @@ graph2 = {
 # Then since there is a cycle 1->2->0->1..., our function should return true, whereas for this other example:
 
 non_cyclic = {
-                1: [2]
-                2: [1,3]
+                1: [2],
+                2: [1,3],
                 3: [2]
              }
 
@@ -22,3 +22,30 @@ non_cyclic = {
 
 # We want to write a function detect_cycle that accepts a graph and returns True if there is a cycle or return False if there does not exist a cycle.
 # To be specific: For any visited vertex 'v', if there is an adjacent 'u' that is already visited and 'u' is not a parent of 'v', then there is a cycle in the graph.
+
+# Idea: Do a depth first search algorithm, starting from any node in the graph. Keep track of "visited" nodes and "parent" nodes. If ever come across a node that is already visited and NOT a parent node if the current node, then return True. else, return False
+
+def detect_cycle(graph):
+    visited = { vertex: False for vertex in graph }
+
+    for vertex in graph:
+        if visited[vertex] is False:
+            if detect_cycle_util(graph, vertex, visited) is True:
+                return True
+
+    return False
+
+def detect_cycle_util(graph, vertex, visited, parent = None):
+    visited[vertex] = True
+
+    for adjacent_vertex in graph[vertex]:
+        if visited[adjacent_vertex] is False:
+            if detect_cycle_util(graph, adjacent_vertex, visited, vertex):
+                return True
+        elif parent != adjacent_vertex:
+            return True
+
+    return False
+
+print(detect_cycle(graph2))
+print(detect_cycle(non_cyclic))
