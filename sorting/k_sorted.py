2f40a074e7ff7950da821785214fec6c1b348fcb
Young Ju Park <parkyngj@gmail.com>
2017-01-22 20:08:53 -0800

    adds k sorted solution

--- sorting/k_sorted.py
+++ sorting/k_sorted.py
@@ -9,4 +9,21 @@
     # Make a heap of size k+1 from the first k+1 elements
     # for i in len(array):
         # extract from heap and put it at position k of output array
-        # add element at position i + k + 1 to heap
+        # add element at position i + k + 2 to heap
+
+# Temporary work around for relative import for Python
+from heap import MinHeap
+
+def sort_k_sorted(array,k):
+    sorted = []
+    min_heap = MinHeap(array[:k+2])
+
+    for i in range(len(array)):
+        print(min_heap.heap)
+        sorted.append(min_heap.extract_min())
+        if i+k+2 < len(array):
+            min_heap.insert(array[i+k+2])
+
+    return sorted
+
+print(sort_k_sorted([2, 6, 3, 12, 56, 8], 2))
